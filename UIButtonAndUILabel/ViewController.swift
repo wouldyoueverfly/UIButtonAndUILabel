a19fd0d4a24be4afbffb33699dab93873b9646be
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-22 21:03:14 +0300

    Initial Commit

---
+++ UIButtonAndUILabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIButtonAndUILabel
+//
+//  Created by Ruslan Khusainov on 22.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
