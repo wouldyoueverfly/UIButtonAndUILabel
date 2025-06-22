5dfdfe4a0d86f98fe7c0315f18906a3246256d00
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-22 22:47:19 +0300

    []

--- UIButtonAndUILabel/ViewController.swift
+++ UIButtonAndUILabel/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet var actionButtons: [UIButton]!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        
+        
+        button.isHidden = true
+        for button in actionButtons {
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+        }
+        
+        
+        button.setTitle("Clear", for: UIControl.State.normal)
+        button.setTitleColor(.white, for: UIControl.State.normal)
+        button.backgroundColor = .red
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.tag == 0 {
+            label.text = "Hello, World!"
+            label.textColor = .red
+        } else if sender.tag == 1 {
+            label.text = "Vroom"
+            label.textColor = .blue
+        } else if sender.tag == 2 {
+            label.isHidden = true
+            button.isHidden = true
+        }
+    }
+    
 }
 
